9867bbb98c527bbe9b82dd7afa719f04671b2258
AbuseClinic <abuseclinic@gmail.com>
2026-01-02 14:27:23 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, (process as any).cwd(), '');
   
   return {
     plugins: [react()],
     define: {
+      // Define process.env.API_KEY globally for the browser
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
     build: {
       outDir: 'dist',
+      
+      // 1. Naikkan batas peringatan menjadi 1000kb (agar warning hilang)
       chunkSizeWarningLimit: 1000,
+
+      // 2. Konfigurasi manualChunks menggunakan FUNGSI (Lebih ampuh)
       rollupOptions: {
         output: {
-          // Using the Function syntax from Rollup docs
           manualChunks(id) {
-            // If the module is inside node_modules, put it in a chunk named 'vendor'
+            // Jika file berasal dari folder 'node_modules', pindahkan ke file terpisah
             if (id.includes('node_modules')) {
               return 'vendor';
             }
